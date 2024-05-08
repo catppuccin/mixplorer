5f8da3e6060da675bea142a16aecd714154308a0
auriafoxgirl <53555592+auriafoxgirl@users.noreply.github.com>
2024-05-08 16:30:49 +0300

    initial commit

---
+++ zip.py
@@ -0,0 +1,19 @@
+import os
+from zipfile import ZipFile
+
+flavors = ['latte', 'frappe', 'macchiato', 'mocha']
+for flavor in flavors:
+   file = open('output/' + flavor + '.xml', 'r')
+   theme = file.read()
+   file.close()
+   file = open('output/' + flavor + '.txt', 'r')
+   accents = file.read()
+   file.close()
+   with ZipFile('output/mixplorer-catppuccin-' + flavor + '.zip', 'w') as flavor_zip:
+      for accent in accents.split('\n'):
+      	accentData = accent.split(':') # name, color
+      	with ZipFile('output/temp.zip', 'w') as zip_obj:
+         	zip_obj.writestr('properties.xml', theme.replace('${accent}', accentData[1]).replace('${accentName}', accentData[0]))
+      	flavor_zip.write('output/temp.zip', 'mixplorer-catppuccin-' + flavor + '-' + accentData[0] + '.mit')
+   print('saved output/mixplorer-catppuccin-'  + flavor + '.zip')
+os.remove("output/temp.zip")
