6f7bb15b769e1073ac13a2758040b1911bb9df4c
auriafoxgirl <53555592+auriafoxgirl@users.noreply.github.com>
2024-05-08 21:11:14 +0300

    previews and better code formatting

--- README.md
+++ README.md
@@ -6,32 +6,32 @@
 </h3>
 
 <p align="center">
-	<a href="https://github.com/catppuccin/template/stargazers"><img src="https://img.shields.io/github/stars/catppuccin/template?colorA=363a4f&colorB=b7bdf8&style=for-the-badge"></a>
-	<a href="https://github.com/catppuccin/template/issues"><img src="https://img.shields.io/github/issues/catppuccin/template?colorA=363a4f&colorB=f5a97f&style=for-the-badge"></a>
-	<a href="https://github.com/catppuccin/template/contributors"><img src="https://img.shields.io/github/contributors/catppuccin/template?colorA=363a4f&colorB=a6da95&style=for-the-badge"></a>
+	<a href="https://github.com/catppuccin/template/stargazers"><img src="https://img.shields.io/github/stars/auriafoxgirl/mixplorer?colorA=363a4f&colorB=b7bdf8&style=for-the-badge"></a>
+	<a href="https://github.com/catppuccin/template/issues"><img src="https://img.shields.io/github/issues/auriafoxgirl/mixplorer?colorA=363a4f&colorB=f5a97f&style=for-the-badge"></a>
+	<a href="https://github.com/catppuccin/template/contributors"><img src="https://img.shields.io/github/contributors/auriafoxgirl/mixplorer?colorA=363a4f&colorB=a6da95&style=for-the-badge"></a>
 </p>
 
 <p align="center">
-	<img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/previews/preview.webp"/>
+	<img src="https://raw.githubusercontent.com/auriafoxgirl/mixplorer/main/assets/previews/preview.webp"/>
 </p>
 
 ## Previews
 
 <details>
 <summary>🌻 Latte</summary>
-<img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/previews/latte.webp"/>
+<img src="https://raw.githubusercontent.com/auriafoxgirl/mixplorer/main/assets/previews/latte.webp"/>
 </details>
 <details>
 <summary>🪴 Frappé</summary>
-<img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/previews/frappe.webp"/>
+<img src="https://raw.githubusercontent.com/auriafoxgirl/mixplorer/main/assets/previews/frappe.webp"/>
 </details>
 <details>
 <summary>🌺 Macchiato</summary>
-<img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/previews/macchiato.webp"/>
+<img src="https://raw.githubusercontent.com/auriafoxgirl/mixplorer/main/assets/previews/macchiato.webp"/>
 </details>
 <details>
 <summary>🌿 Mocha</summary>
-<img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/previews/mocha.webp"/>
+<img src="https://raw.githubusercontent.com/auriafoxgirl/mixplorer/main/assets/previews/mocha.webp"/>
 </details>
 
 ## Usage
@@ -49,7 +49,7 @@
 
 ## 💝 Thanks to
 
-- [Auria](https://github.com/auriafoxgirl)
+- [Auriafoxgirl](https://github.com/auriafoxgirl)
 
 &nbsp;
 
--- colors.lua
+++ colors.lua
@@ -1,53 +1,53 @@
 return function(theme)
-   --local accent = theme.mauve
-   local accent = '${accent}' -- replaced later in python script
-   
-   return {
-   	bg_bar_main = '#ff'..theme.base,
-   	tint_popup_bg = '#ff'..theme.mantle,
-   	tint_progress_bar = '#ff'..accent,
-   	tint_tab_indicator_selected = '#ff'..accent,
-   	tint_icon_signs = '#ff'..theme.maroon,
-   	
-   	syntax_keyword = '#ff'..theme.mauve,
-   	syntax_symbol = '#ff'..theme.peach,
-   	syntax_comment = '#ff'..theme.overlay0,
-   	syntax_attr = '#ff'..theme.sapphire,
-   	
-   	text_button = '#dc'..theme.text,
-   	text_edit_box = '#ff'..theme.text,
-   	text_grid_primary = '#ff'..theme.text,
-   	
-   	text_grid_secondary = '#ff'..theme.subtext1,
-   	text_bar_main_secondary = '#ff'..theme.subtext1,
-   	
-   	text_popup_primary = '#e6'..theme.text,
-   	text_popup_secondary = '#96'..theme.text,
-   	
-   	tint_bar_main_icons = '#ff'..theme.subtext1,
-   	tint_popup_controls = '#ff'..accent,
-   	
-   	tint_progress_track = '#ff'..theme.surface2,
-   	highlight_bar_main_buttons = '#ff'..theme.overlay0,
-   	tint_page_separator = '#ff'..theme.base,
-   	highlight_popup_list_item = '#ff'..theme.surface0,
-   	
-   	text_filter_box_hint = '#ff'..theme.subtext0,
-   	
-   	text_link_pressed = '#ff'..theme.text,
-   	
-   	text_edit_selection_background = '#64'..accent,
-   	
-   	highlight_visited_folder = '#0a'..theme.subtext1,
-   	tint_folder = '#ff'..theme.subtext0,
-   	highlight_grid_item = '#80'..accent,
-   	
-   	text_breadcrumb_selected = '#ff'..theme.text,
-   	text_breadcrumb = '#ff'..theme.subtext1,
-   	syntax_string = '#ff'..theme.text,
-   	tint_popup_icons = '#ff'..theme.subtext1,
-   	tint_bar_tools_icons = '#ff'..theme.subtext1,
-   	tint_notification_buttons = '#ff'..theme.surface0,
-   	text_edit_selection_foreground = '#ff'..theme.text,
-   }
+  --local accent = theme.mauve
+  local accent = '${accent}' -- replaced later in python script
+
+  return {
+    bg_bar_main = '#ff'..theme.base,
+    tint_popup_bg = '#ff'..theme.mantle,
+    tint_progress_bar = '#ff'..accent,
+    tint_tab_indicator_selected = '#ff'..accent,
+    tint_icon_signs = '#ff'..theme.maroon,
+
+    syntax_keyword = '#ff'..theme.mauve,
+    syntax_symbol = '#ff'..theme.peach,
+    syntax_comment = '#ff'..theme.overlay0,
+    syntax_attr = '#ff'..theme.sapphire,
+
+    text_button = '#dc'..theme.text,
+    text_edit_box = '#ff'..theme.text,
+    text_grid_primary = '#ff'..theme.text,
+
+    text_grid_secondary = '#ff'..theme.subtext1,
+    text_bar_main_secondary = '#ff'..theme.subtext1,
+
+    text_popup_primary = '#e6'..theme.text,
+    text_popup_secondary = '#96'..theme.text,
+
+    tint_bar_main_icons = '#ff'..theme.subtext1,
+    tint_popup_controls = '#ff'..accent,
+
+    tint_progress_track = '#ff'..theme.surface2,
+    highlight_bar_main_buttons = '#ff'..theme.overlay0,
+    tint_page_separator = '#ff'..theme.base,
+    highlight_popup_list_item = '#ff'..theme.surface0,
+
+    text_filter_box_hint = '#ff'..theme.subtext0,
+
+    text_link_pressed = '#ff'..theme.text,
+
+    text_edit_selection_background = '#64'..accent,
+
+    highlight_visited_folder = '#0a'..theme.subtext1,
+    tint_folder = '#ff'..theme.subtext0,
+    highlight_grid_item = '#80'..accent,
+
+    text_breadcrumb_selected = '#ff'..theme.text,
+    text_breadcrumb = '#ff'..theme.subtext1,
+    syntax_string = '#ff'..theme.text,
+    tint_popup_icons = '#ff'..theme.subtext1,
+    tint_bar_tools_icons = '#ff'..theme.subtext1,
+    tint_notification_buttons = '#ff'..theme.surface0,
+    text_edit_selection_foreground = '#ff'..theme.text,
+  }
 end
